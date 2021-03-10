bb07f3988bdb8c2cc34915b02df3f3ab3d32b3cb
DaveyPlugers <daveyplugers@gmail.com>
2021-03-10 03:22:20 +0100

    Adjusted to fit Coding Guideline + general cleaning of the code

--- CompProject1.py
+++ CompProject1.py
@@ -1,3 +1,8 @@
+"""
+
+
+"""
+
 #Framework of Allposition array: Allposition[i][j][k] =>
 #i gives timestep [Timestep0,Timestep1,Timestep2,...] Will be appended like that where =>
 #j gives particle number Timestep0 = [Particle1,Particle2] where =>
@@ -10,54 +15,52 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 
-Temperature = 50  #Make this an input variable later
+Temperature = 0.5  #Make this an input variable later
 Density= 1.2 #In units, atoms/sigma**3, make input variable later
-mass=1 #Mass in atomic mass units
-
-
-
-Tsteplength = 0.001
-BoxSize = 3*(4*mass/Density)**(1/3)  #Times 4 since 4 particles per cube
+Mass=1 #Mass in atomic mass units
+BoxSize = 3 * (4 * Mass / Density) ** (1 / 3)  #Times 4 since 4 particles per cube
 print("Boxsize = " + str(BoxSize))
-Bins = 20
-Timesteps = 400
+TimeSteps = 400
+TimeStepLength = 0.001
+
+HistBins = 20
 HistStart = 100
-HistTimes = [HistStart + (1+x)*50 for x in range(int((Timesteps-HistStart)/50))]
-print(HistTimes)
+HistTimes = [HistStart + (1+x) * 50 for x in range(int((TimeSteps - HistStart) / 50))]
+RescaleTimes = [15,50,80,150]
+PressureTimes = [20, 80, 200, 250, 300,350]
 
-Random = False
-#Generating particles and Allpositions array
+
+RandomInitialisation = False
+CreatePlots = True
+
 Particles = []
-if Random:
-    Particleamount = 8
-    dimension = 2
+if RandomInitialisation:
+    ParticleAmount = 8
+    Dimension = 2
     Particlevelocity = 0.001
-    for l in range(Particleamount):
+    for l in range(ParticleAmount):
 
-        Partic = [[BoxSize*random.random() for d in range(dimension)],[2*Particlevelocity*(random.random()-0.5) for d in range(dimension)]]
+        Partic = [[BoxSize * random.random() for d in range(Dimension)], [2 * Particlevelocity * (random.random() - 0.5) for d in range(Dimension)]]
         Particles.append(Partic)
-
-
-
 else:
-    dimension=3
-    Particleamount = 108
-    Number = 0
-    var = (Temperature / 100) ** (1 / 2)  # 100 comes from eps/k_b = 100K
-    Velocities = np.random.normal(0, var, 324)
+    Dimension=3
+    ParticleAmount = 108
+    ParticleNumber = 0
+    VelVariance = (Temperature) ** (1 / 2)
+    Velocities = np.random.normal(0, VelVariance, 324)
     for i in range(6):
         for j in range(6):
             for k in range(3):
-                Partic = [[k * BoxSize / 3 + (j + i) % 2 * BoxSize / 6, j * BoxSize / 6, i * BoxSize / 6],
-                          [Velocities[3 * Number], Velocities[3 * Number + 1], Velocities[3 * Number + 2]]]
-                Number += 1
+                Partic = [[k * BoxSize / 3 + (j + i) % 2 * BoxSize / 6, j * BoxSize / 6, i * BoxSize / 6], #Modulo operators create FCC lattice structure
+                          [Velocities[3 * ParticleNumber], Velocities[3 * ParticleNumber + 1], Velocities[3 * ParticleNumber + 2]]]
+                ParticleNumber += 1
                 Particles.append(Partic)
 Particles = np.array(Particles).astype('float64')
-Allpositions = [[] for x in range(Timesteps+1)]  #Premaking is faster than appending in Matlab not sure about Python
-Allpositions[0] = Particles.copy()
+AllPositions = [[] for x in range(TimeSteps + 1)]  #Premaking is faster than appending in Matlab, not sure about Python
+AllPositions[0] = Particles.copy()
 
 
-def Distancepoints(Vector1,Vector2):
+def DistancePoints(Vector1,Vector2):
     '''
     Takes 2 position vectors and calculates distance between them
     :param Vector1: position vector for particle one
@@ -69,56 +72,55 @@ def Distancepoints(Vector1,Vector2):
 
 
 
-def LJForce(Vector1,Vector2):
+def VecPairLJForce(Vector1,Vector2):
     '''
     Takes 2 position vectors and returns the Lennard-Jones force on Vector1 due to Vector2
     :param Vector1: position vector for particle one
     :param Vector2: position vector for particle two
     :return: Lennard-Jones force vector between two particles
     '''
-    r = Distancepoints(Vector1,Vector2)
-    Constant = 24*(2/r**14 - 1/r**8)  #Neglect - since it's in the force, extra 1/r for normalisation Vector1 - Vector2 in next line
-    Force = [Constant*x for x in ((Vector1 - Vector2 + BoxSize/2)%BoxSize - BoxSize/2)] #Maybe switch 2 and 1
+    Dist = DistancePoints(Vector1,Vector2)
+    Constant = 24*(2/Dist**14 - 1/Dist**8)  #Extra 1/r for normalisation Vector1 - Vector2 in next line
+    Force = [Constant*x for x in ((Vector1 - Vector2 + BoxSize/2)%BoxSize - BoxSize/2)]
     return Force
 
 
-def Force(j,tstep):
+def TotalForce(j,tstep):
     '''
     This calculates the total force vector experienced by particle j
     :param j: index of a particle
-    :param tstep: time step
-    :return:total force
+    :param tstep: time step with a special case for tstep==-1 for the updated values that aren't saved yet
+    :return: Total force on particle j
     '''
-    #Can be improved to exploit symmetry of the force (LJForce(j,i) = LJForce(i,j))
-    force = np.array([0 for l in range(dimension)], dtype=np.float64)
-    if tstep ==-1: #We don't save position tstep+1 until we have done the velocities as well, this is a workaround to get the updated force
-        for i in range(Particleamount):
-            if i !=j:
-                force += LJForce(Particles[j,0], Particles[i,0])
-    else:
-        for i in range(Particleamount):
-            if i !=j:
-                force += LJForce(Allpositions[tstep][j,0], Allpositions[tstep][i][0])    #We use the Allpositions instead of Particles since these contain already updated values
 
-    return force
+    TotalForce = np.array([0 for l in range(Dimension)], dtype=np.float64)
+    if tstep ==-1: #This is a workaround to get the updated position and thus updated force
+        for i in range(ParticleAmount):
+            if i !=j:
+                TotalForce += VecPairLJForce(Particles[j,0], Particles[i,0])
+    else:
+        for i in range(ParticleAmount):
+            if i !=j:
+                TotalForce += VecPairLJForce(AllPositions[tstep][j, 0], AllPositions[tstep][i][0])    #We use the Allpositions instead of Particles since we need the old positions
+
+    return TotalForce
+
 
 Epot = []
 Ekin = []
 Etot = []
-
-
 def Energy():
     '''
-    Can be called to save the potential,kinetic and total energy of the system
+    Can be called to save the potential,kinetic and total energy of the system at the current timestep
     '''
     pot=0
     kin=0
-    for j in range(Particleamount):
+    for j in range(ParticleAmount):
         pot1 = 0
-        kin += (0.5*mass)*(sum([x*x for x in Particles[j,1]]))
-        for k in range(Particleamount):
+        kin += (0.5 * Mass) * (sum([x * x for x in Particles[j, 1]]))
+        for k in range(ParticleAmount):
             if k !=j:
-                r = Distancepoints(Particles[j,0],Particles[k,0])
+                r = DistancePoints(Particles[j,0],Particles[k,0])
                 pot1 += 4*((1/r)**12 - (1/r)**6)
         pot += pot1/2  # *0.5 to prevent double counting
     Epot.append(pot)
@@ -132,34 +134,37 @@ def Rescale():
     :return: Scale factor for the velocities
     '''
     VelSquaredSummed = 0
-    for j in range(Particleamount):
+    for j in range(ParticleAmount):
         VelSquaredSummed += sum([x*x for x in Particles[j,1]])
-    Lambda = (((Particleamount-1)*3*Temperature)/(VelSquaredSummed*100))**(1/2)
+    Lambda = (((ParticleAmount - 1) * 3 * Temperature) /VelSquaredSummed) ** (1 / 2)
     return Lambda
 
 def Histogram(Bins):
     '''
-    Can be called to calculate a histogram giving number of particles in certain ranges
+    Can be called to calculate a histogram giving number of particle pairs at certain distances
     :param Bins: Amount of intervals of the histogram
     :return: An array of Histogram values
     '''
     Histo = np.histogram([BoxSize], bins=[BoxSize / (2 * Bins) * x for x in range(Bins + 1)])[0]
-    for i in range(Particleamount):
-        Distances = [Distancepoints(Particles[1+i+j,0],Particles[i,0]) for j in range(Particleamount-i-1)]  #Skipping some Distance calculations here but compensating the counting by a factor 2 in the end
+    for i in range(ParticleAmount):
+        Distances = [DistancePoints(Particles[1+i+j,0],Particles[i,0]) for j in range(ParticleAmount - i - 1)]
         Histo += np.histogram(Distances, bins=[BoxSize/(2*Bins)*x for x in range(Bins+1)])[0]
     #Histo = 2*Histo
     return Histo
 
 
 def Pressure():
+    '''
+    Can be called to save the pressure of the system at the current timestep
+    '''
     DistTimesForce = []
-    for i in range(Particleamount):
-        for j in range(Particleamount-i-1):
-            r = Distancepoints(Particles[1+i+j,0],Particles[i,0])
-            DistTimesForce.append(12*(-2/r**12 + 1/r**6))
-    Pressure = 1 - (np.average(np.array(DistTimesForce))/(3*Particleamount*Temperature/100))
+    for i in range(ParticleAmount):
+        for j in range(ParticleAmount - i - 1):
+            Dist = DistancePoints(Particles[1+i+j,0],Particles[i,0])
+            DistTimesForce.append(12*(-2/Dist**12 + 1/Dist**6))
+    Pressure = 1 - (np.average(np.array(DistTimesForce)) / (3 * ParticleAmount * Temperature))
     print(np.average(np.array(DistTimesForce)))
-    print(r)
+    print(Dist)
     return Pressure
 
 
@@ -167,82 +172,65 @@ def Pressure():
 
 
 
-for tstep in range(Timesteps):      #Here we let the magic happen, we loop and let the system evolve
-
-    for j in range(Particleamount): #We make sure to update all the particle positions
-
-        Particles[j,0] = Particles[j,0] + Particles[j,1]*Tsteplength +Tsteplength**2/(2*mass)*Force(j,tstep)
+for tstep in range(TimeSteps):
+    print("timestep = " + str(tstep))
+    for j in range(ParticleAmount):
+        Particles[j,0] = Particles[j,0] + Particles[j,1] * TimeStepLength + TimeStepLength ** 2 / (2 * Mass) * TotalForce(j, tstep)
         Particles[j,0] = Particles[j,0] %BoxSize
+    for j in range(ParticleAmount):
+        Particles[j,1] = Particles[j,1] + TimeStepLength / (2 * Mass) * (TotalForce(j, -1) + TotalForce(j, tstep))
 
-    for j in range(Particleamount): #Here we update the particle velocities
-        Particles[j,1] = Particles[j,1] + Tsteplength/(2*mass)*(Force(j,-1) + Force(j,tstep))
 
-
-    if tstep%10==0: #For now I do every 10 steps
+    if tstep%10==0:
         Energy()
-    if tstep%50==0 or tstep in [10,20,35,80]:
+
+    if tstep in RescaleTimes:
         ScaleFactor = Rescale()
         print("Scalefactor= " + str(ScaleFactor))
-        for k in range(Particleamount):
+        for k in range(ParticleAmount):
             Particles[k,1] = ScaleFactor*Particles[k,1]
-    if tstep ==HistStart:
-        Histo = Histogram(Bins)
 
+    if tstep ==HistStart:
+        Histo = Histogram(HistBins)
     elif tstep in HistTimes:
-        Histo += Histogram(Bins)
+        Histo += Histogram(HistBins)
         print(Histo)
-    if tstep in [20, 80, 200, 250, 300,350]:
+
+    if tstep in PressureTimes:
         print("Pressure = " + str(Pressure()))
 
+    AllPositions[tstep + 1] = (Particles.copy())
 
-
-
-    Allpositions[tstep + 1] = (Particles.copy())       #We save the updated values in our list
-
-    print("timestep = " + str(tstep))            #Not needed but it's here anyways
 
 Histo = Histo*(1/(len(HistTimes)))
 print(Histo)
-BinSize = BoxSize/(2*Bins)
-PairCorrelationDifferent = [(2*BoxSize**3*Histo[i])/(Particleamount*(Particleamount-1)*4*math.pi*((i+1)*BinSize)**2*BinSize) for i in range(len(Histo))]
+
+BinSize = BoxSize/(2 * HistBins)
+PairCorrelationDifferent = [(2*BoxSize**3*Histo[i]) / (ParticleAmount * (ParticleAmount - 1) * 4 * math.pi * ((i + 1) * BinSize) ** 2 * BinSize) for i in range(len(Histo))]
 print("(r+Binsize)^2 instead " + str(PairCorrelationDifferent))
 
-PairCorrelation = [(2*BoxSize**3*Histo[i+1])/(Particleamount*(Particleamount-1)*4*math.pi*((i+1)*BinSize)**2*BinSize) for i in range(len(Histo)-1)]
+PairCorrelation = [(2*BoxSize**3*Histo[i+1]) / (ParticleAmount * (ParticleAmount - 1) * 4 * math.pi * ((i + 1) * BinSize) ** 2 * BinSize) for i in range(len(Histo) - 1)]
 print("Skipped first one" + str(PairCorrelation))
 
 
 
 #print(Allpositions[0:5]) #Used for checking mistakes in simulation, remove later
 
-
-plot2 = plt.figure(2)
-plt.plot(range(0,Timesteps,10),Epot)
-plt.plot(range(0,Timesteps,10),Ekin)
-plt.plot(range(0,Timesteps,10),Etot)
-plt.legend(["Epot","Ekin","Etot"])
-
-
-
-#Attempt at making animation
-
-fig, ax = plt.subplots()
-
-ln1, = plt.plot([], [], 'ro')
+if CreatePlots:
+    plot2 = plt.figure(2)
+    plt.plot(range(0, TimeSteps, 10), Epot)
+    plt.plot(range(0, TimeSteps, 10), Ekin)
+    plt.plot(range(0, TimeSteps, 10), Etot)
+    plt.legend(["Epot","Ekin","Etot"])
 
 
-
-
-def init():
-    ax.set_xlim(0, BoxSize)
-    ax.set_ylim(0, BoxSize)
-
-
-#The 100 I divided and multiplied with is something I will have to manually change, it's so our animation isn't slow when we have many steps
-
-
-def update(q):
-    ln1.set_data([Allpositions[int(10*q)][:,0,0]], [Allpositions[int(10*q)][:,0,1]])   #i is the timestep, 0 is particle 1, 0 is for position not velocity, 0 is for x not y, second one we have 1 in the last for y not x
-
-ani = FuncAnimation(fig, update,frames=int((Timesteps)/10+1),interval=10, init_func=init)
-plt.show()
+    fig, ax = plt.subplots()
+    ln1, = plt.plot([], [], 'ro')
+    def init():
+        ax.set_xlim(0, BoxSize)
+        ax.set_ylim(0, BoxSize)
+    def update(q):
+        ln1.set_data([AllPositions[int(10 * q)][:, 0, 0]], [AllPositions[int(10 * q)][:, 0, 1]])
+    ani = FuncAnimation(fig, update, frames=int((TimeSteps) / 10 + 1), interval=10, init_func=init)
+    plt.show()
 
