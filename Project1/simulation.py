cea8958085d106295bb607da76767655b5f6e697
DaveyPlugers <71390708+DaveyPlugers@users.noreply.github.com>
2021-03-18 16:27:59 +0100

    Histbins 20 -> 50

--- Project1/simulation.py
+++ Project1/simulation.py
@@ -121,7 +121,7 @@ if Animation:
 BoxSize = 3 * (4 / Density) ** (1 / 3)  # Times 4 since 4 particles per cube
 TimeStepLength = 0.001
 
-HistBins = 20
+HistBins = 50
 HistStart = 100
 HistTimes = np.linspace(HistStart, TimeSteps, num=int(((TimeSteps - HistStart) / 50)) + 1)
 RescaleTimes = np.linspace(100, TimeSteps, num=int(TimeSteps / 100))
