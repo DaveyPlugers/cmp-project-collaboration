af8ea949eb16c20bc5f533e1848183052f9d4192
DaveyPlugers <daveyplugers@gmail.com>
2021-04-19 20:41:55 +0200

    Made multiple simulations for t_equi, did some renaming

--- Project2/main.py
+++ Project2/main.py
@@ -6,19 +6,24 @@ from tqdm import tqdm
 
 J=1
 KB = 1
-T=3
-Number = 50
-NumberSquared = Number**2
-Repetitions = NumberSquared*800
-#Time in units of steps per lattice
-t_equilibrium = 500
+Temperature=2.8
+Lattice = 50
+LatticeSquared = Lattice**2
+Repetitions = LatticeSquared*5000
+#Time in units of steps per site
+t_equilibrium = 3000
 t_correlation = 5
 Amount_Blocks = 4
-Correlation_Mode = False
+Correlation_Mode = True
 
 
-
-Spin_Array = np.array([[int(2*(random.randint(0,1)-0.5)) for x in range(Number)]for y in range(Number)])
+def Random_Initialisation():
+    """
+    Can be called to return an initialisation with spin up or down randomly distributed
+    :return: np.array consisting of -1 or 1 representing the spins
+    """
+    return np.array([[int(2*(random.randint(0,1)-0.5)) for x in range(Lattice)]for y in range(Lattice)])
+Spin_Array = Random_Initialisation()
 
 def Energy_Calculation():
     """
@@ -26,9 +31,9 @@ def Energy_Calculation():
     :return: Scalar value of the total energy of the system
     """
     Energy = 0
-    for x in range(Number):
-        for y in range(Number):
-            Energy += -0.5*J*Spin_Array[y,x]*(Spin_Array[y,(x+1)%Number] + Spin_Array[y,(x-1)%Number] + Spin_Array[(y+1)%Number,x] + Spin_Array[(y-1)%Number,x])
+    for x in range(Lattice):
+        for y in range(Lattice):
+            Energy += -0.5*J*Spin_Array[y,x]*(Spin_Array[y,(x+1)%Lattice] + Spin_Array[y,(x-1)%Lattice] + Spin_Array[(y+1)%Lattice,x] + Spin_Array[(y-1)%Lattice,x])
     return Energy
 Energy = Energy_Calculation()
 
@@ -39,10 +44,10 @@ def Energy_Difference(x_index,y_index):
     :param y_index: y position of the spin
     :return: Scalar energy difference if the spin is flipped
     """
-    Energy_Diff = 2*J*Spin_Array[y_index,x_index]*(Spin_Array[y_index,(x_index+1)%Number] + Spin_Array[y_index,(x_index-1)%Number] + Spin_Array[(y_index+1)%Number,x_index] + Spin_Array[(y_index-1)%Number,x_index])
+    Energy_Diff = 2*J*Spin_Array[y_index,x_index]*(Spin_Array[y_index,(x_index+1)%Lattice] + Spin_Array[y_index,(x_index-1)%Lattice] + Spin_Array[(y_index+1)%Lattice,x_index] + Spin_Array[(y_index-1)%Lattice,x_index])
     return Energy_Diff
 
-Probabilities = [math.exp(-4/(KB*T)),math.exp(-8/(KB*T))]
+Probabilities = [math.exp(-4/(KB*Temperature)),math.exp(-8/(KB*Temperature))]
 def Flip_Calculator(Delta_Energy):
     """
     Takes energy difference, calculates whether the spin flips with probability e^(-Delta_Energy/(kb*T))
@@ -55,14 +60,14 @@ def Flip_Calculator(Delta_Energy):
         Boolean = random.random() < Probabilities[1]
     return Boolean
 
-Spin_Magn = np.array([])
+
 def Spin_Magnetization(Spin_Magn):
     """
     Takes the array of Spin Magnetisations, returns a version appended with the new value of mean magn. per spin
     :param Spin_Magn: Array of mean magn. per spin
     :return: Same array with additionally the current mean magn. per spin
     """
-    return np.append(Spin_Magn,[np.sum(Spin_Array)/Number**2])
+    return np.append(Spin_Magn,[np.sum(Spin_Array)/Lattice**2])
 
 
 def Auto_Correlation(t_equilibrium,Spin_Magn):
@@ -98,11 +103,21 @@ def Correlation_Time(Auto_Corr):
     return Tau
 
 def Magnetic_Suscept(Magnetisation_Array):
-    return 1/(KB*T*NumberSquared)*(np.mean(Magnetisation_Array**2)-np.mean(Magnetisation_Array)**2)
+    """
+    Takes an array of total magnetisations, returns the Magnetic Susceptibility calculated from these values
+    :param Magnetisation_Array: Array that has the total magnetisation of the system at different times (in our case 8 values)
+    :return: Scalar value of the approximated Magnetic Susceptibility
+    """
+    return 1/(KB*Temperature*LatticeSquared)*(np.mean(Magnetisation_Array**2)-np.mean(Magnetisation_Array)**2)
 
 
 def Specific_Heat(Energy_Array):
-    return 1 / (KB * NumberSquared * T**2) * (np.mean(Energy_Array ** 2) - np.mean(Energy_Array) ** 2)
+    """
+    Takes an array of total energy, returns the Specific Heat calculated from these values
+    :param Energy_Array: Array that has the total energy of the system at different times (in our case 8 values)
+    :return: Scalar value of the approximated Specific Heat
+    """
+    return 1 / (KB * LatticeSquared * Temperature**2) * (np.mean(Energy_Array ** 2) - np.mean(Energy_Array) ** 2)
 #New structureplan: Have 2 different simulation possibilities:
 # 1) Run multiple simulations on the same temperature, different initials, use these to calculate equilibrium and correlation time
 # 2) Simulation where you give t_equi and t_corr, then make it run until t_equi, then start dividing in k blocks of 16t_corr
@@ -114,22 +129,34 @@ def Specific_Heat(Energy_Array):
 
 if Correlation_Mode:
     bar = tqdm(range(Repetitions)) #Running full simulation
+    Amount_Initialisations = 4
+    Multiple_Spin_Magn = [0,0,0,0]
 else:
-    bar = tqdm(range(t_equilibrium * NumberSquared)) #Running untill equilibrium then start a continuation for physical values later
+    bar = tqdm(range(t_equilibrium * LatticeSquared)) #Running untill equilibrium then start a continuation for physical values later
+    Amount_Initialisations = 1
 
-for a in bar:
-    x_index = random.randint(0,Number-1)
-    y_index = random.randint(0,Number-1)
+for z in range(Amount_Initialisations):
+    Spin_Magn = np.array([])
+    for a in bar:
+        x_index = random.randint(0,Lattice-1)
+        y_index = random.randint(0,Lattice-1)
 
-    Delta_Energy = Energy_Difference(x_index,y_index)
-    if Delta_Energy <= 0:
-        Spin_Array[y_index,x_index] = -Spin_Array[y_index,x_index]
-        Energy += Delta_Energy
-    elif Flip_Calculator(Delta_Energy):
-        Spin_Array[y_index,x_index] = -Spin_Array[y_index,x_index]
-        Energy += Delta_Energy
-    if a%NumberSquared == 0:
-        Spin_Magn = Spin_Magnetization(Spin_Magn)
+        Delta_Energy = Energy_Difference(x_index,y_index)
+        if Delta_Energy <= 0:
+            Spin_Array[y_index,x_index] = -Spin_Array[y_index,x_index]
+            Energy += Delta_Energy
+        elif Flip_Calculator(Delta_Energy):
+            Spin_Array[y_index,x_index] = -Spin_Array[y_index,x_index]
+            Energy += Delta_Energy
+        if a%LatticeSquared == 0:
+            Spin_Magn = Spin_Magnetization(Spin_Magn)
+    if Correlation_Mode:
+        Multiple_Spin_Magn[z] = Spin_Magn
+        if not z==3:
+            Spin_Array = Random_Initialisation()
+            bar = tqdm(range(Repetitions))
+
+
 
 
 if not Correlation_Mode:
@@ -141,12 +168,12 @@ if not Correlation_Mode:
     Energy_Array = np.array([]) #Can use Energy_PSpin too but this is easier
     Magnetisation_Array = np.array([])
 
-    Measurement_Rate = math.ceil(2*t_correlation*NumberSquared)
+    Measurement_Rate = math.ceil(2*t_correlation*LatticeSquared)
     bar = tqdm(range(8*Amount_Blocks))
     for i in bar: #Create blocks of length 16*t_corr each consisting of 8 individual measurements every 2*t_corr
         for a in range(Measurement_Rate):
-            x_index = random.randint(0, Number - 1)
-            y_index = random.randint(0, Number - 1)
+            x_index = random.randint(0, Lattice - 1)
+            y_index = random.randint(0, Lattice - 1)
 
             Delta_Energy = Energy_Difference(x_index, y_index)
             if Delta_Energy <= 0:
@@ -156,8 +183,8 @@ if not Correlation_Mode:
                 Spin_Array[y_index, x_index] = -Spin_Array[y_index, x_index]
                 Energy += Delta_Energy
         Sum_Of_Spins = np.sum(Spin_Array)
-        Magn_PSpin.append(abs(Sum_Of_Spins)/Number**2)
-        Energy_PSpin.append(Energy/Number**2)
+        Magn_PSpin.append(abs(Sum_Of_Spins)/Lattice**2)
+        Energy_PSpin.append(Energy/Lattice**2)
         Energy_Array = np.append(Energy_Array,Energy)
         Magnetisation_Array = np.append(Magnetisation_Array,Sum_Of_Spins)
         if (i+1)%8 ==0:
@@ -173,21 +200,26 @@ if not Correlation_Mode:
 
 
 
-print(len(Spin_Magn))
+
 if Correlation_Mode:
-    if t_equilibrium < Repetitions/NumberSquared:
+    if t_equilibrium < Repetitions/LatticeSquared:
         Auto_Corr = Auto_Correlation(t_equilibrium,Spin_Magn)
         Calculated_Correlation_Tau = Correlation_Time(Auto_Corr)
         print(Calculated_Correlation_Tau)
         plt.figure(3)
         plt.plot(Auto_Corr)
-
+    plt.figure(2)
+    Colours = ['r','b','g','k']
+    for z in range(4):
+        plt.plot(Multiple_Spin_Magn[z],Colours[z])
+else:
+    plt.figure(2)
+    plt.plot(Spin_Magn)
 
 plt.figure(1)
 imgplot = plt.imshow(Spin_Array)
 plt.colorbar()
-plt.figure(2)
-plt.plot(Spin_Magn)
+
 
 plt.show()
 
